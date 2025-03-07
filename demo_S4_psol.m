571dedbf82a46d5ccff730842409c689b362177f
Jan Sieber <j.sieber@exeter.ac.uk>
2025-03-07 02:09:25 +0000

    small changes

--- demo_S4_psol.m
+++ demo_S4_psol.m
@@ -15,9 +15,14 @@ n_osc=4;
 parnames={'a','c_ext','delta','tau_s','tau_c'};
 cind=[parnames;num2cell(1:length(parnames))];
 ip=struct(cind{:});
-funcs=set_funcs('wrap_rhs',@(x,p)rhs_coupled_oscillators(0,ip,n_osc,x,p),...
-    'wrap_dirderi',{@(x,p,dx,dp)rhs_coupled_oscillators(1,ip,n_osc,x,p,dx,dp),...
-                    @(x,p,dx,dp)rhs_coupled_oscillators(2,ip,n_osc,x,p,dx,dp)},...
+Cmat=ones(n_osc,n_osc)-n_osc*eye(n_osc);
+on=ones(n_osc,1);
+f=@(x0,x1,x2,a,c,d)d(on,:)-a(on,:).*x1-x0.^3+c(on,:).*(Cmat*x2);
+df={@(x0,x1,x2,a,c,d,dx0,dx1,dx2,da,dc,dd)...
+    dd(on,:)-da(on,:).*x1-a(on,:).*dx1-3*x0.^2.*dx0+dc(on,:).*(Cmat*x2)+c(on,:).*(Cmat*dx2),...
+    @(x0,x1,x2,d,a,c,dx0,dx1,dx2,da,dc,dd)...
+    -2*da(on,:).*dx1-6*x0.*dx0.^2+2*dc(on,:).*(Cmat*dx2)};
+funcs=set_funcs('sys_rhs',{'delay',[ip.a,ip.c_ext,ip.delta],f},'sys_dirderi',df,...)
     'sys_tau',@()[ip.tau_s,ip.tau_c],'x_vectorized',true,'p_vectorized',true);
 %% Initial guess and parameters
 % For periodic orbits we look at the case of diffusive coupling with
@@ -42,40 +47,53 @@ ex1234=permfix('stst','eq1234_0',n_osc,'x','perm',[1,2,3,4]);
     'contpar',ip.tau_c,'step',0.1,parbd{:},...
     'extracolumns','auto','extra_condition',true,...
     'usercond',ex1234,'outputfuncs',true);
-figure(1);clf;ax1=gca;set(ax1,'fontsize',16);
-xlabel(ax1,'\tau_c');ylabel(ax1,'max(mean(x))');
+figure(1);clf;ax1=gca;hold(ax1,'on');set(ax1,'fontsize',16);
+xlabel(ax1,'\tau_c');ylabel(ax1,'max(mean(x1))');
 eq_1234=br_contn(fun_1234,eq_1234,10,'ax',ax1);
 [eq_1234ref,eq_1234tests,eq_1234bifs,eq_1234bifind]=...
     MonitorChange(fun_1234,eq_1234,'min_iterations',5);
 fprintf('1st Hopf bifurcation at tau_c=%g\neigenvalues, eigenvectors:',eq_1234bifs(1).parameter(ip.tau_c));
 disp(round(eq_1234bifs(1).stability.l0,5))
+Plot2dBranch(eq_1234ref,'funcs',fun_1234,'max_nunst',2,'ax',ax1);
+legend(ax1,'Fontsize',16);
+ylim(ax1,[0,0.5]);
 %% Find a few symmetries that one can branch off into
 % The function check_hopf below should return 2 zero singular value to
 % satisfy the equivariant Hopf bifurcation theorem
 check_hopf=@(cnd)SetupHopf(funcs,eq_1234ref,eq_1234bifind,...
     'initcond',cnd,'usercond',cnd,'output','sv','outputfuncs',true);
-% permutation (1234)_0 (time shift 0)
-ev1234_0=permfix('stst','p1234_1_4',n_osc,'v','perm',[1,2,3,4],'rotation',[0,1]);
+%% Permutation (1234)_0 (time shift 0)
+ev1234_0=permfix('stst','p1234_1_4',n_osc,'v',...
+    'perm',[1,2,3,4],'rotation',[0,1]);
 sv_1234_0=check_hopf(ev1234_0)'
-% (1234)_{1/4} (time shift 1/4)
-ev1234_1_4=permfix('stst','p1234_1_4',n_osc,'v','perm',[1,2,3,4],'rotation',[1,4]);
-sv_1234_1_4=check_hopf(ev1234_1_4)'
-% (123)_0 (time shift 0)
-ev123=permfix('stst','pv123',n_osc,'v','perm',[1,2,3],'rotation',[0,1]);
-sv_123=check_hopf(ev123)'
-%(12)_0(34)_0:  1=2 (time shift 0),3=4 (time shift 0), 1=3 (time shift 1/2)
-ev12_34=[permfix('stst','pv12_0_1',n_osc,'v','perm',[1,2],'rotation',[0,1]),...
-         permfix('stst','pv34_0_1',n_osc,'v','perm',[3,4],'rotation',[0,1])];
-sv_12_34=check_hopf(ev12_34)'
-%1=2 (time shift 1/2),3=4 (time shift 0)
-ev12s_34=[permfix('stst','p12_1_2',n_osc,'v','perm',[1,2],'rotation',[1,2]),...
-          permfix('stst','p34_0_1',n_osc,'v','perm',[3,4],'rotation',[0,1])];
-sv_12s_34=check_hopf(ev12s_34)'
-%1=2 only is not enough: we have four zero singular values for time shifts
+%% Permutation (12)_0 only is not enough: we have four zero singular values for time shifts
 %0 or 1/2. Time shift 1/4 would be ok
-ev12=permfix('stst','p12',n_osc,'v','perm',[1,2],'rotation',[0,1]);
+ev12=permfix('stst','p12',n_osc,'v',...
+    'perm',[1,2],'rotation',[0,1]);
 sv_12=check_hopf(ev12)'
-ev12_1_4=permfix('stst','p12',n_osc,'v','perm',[1,2],'rotation',[1,4]);
+%% Permutation (1234)_{1/4} (time shift 1/4)
+ev1234_1_4=permfix('stst','p1234_1_4',n_osc,...
+    'v','perm',[1,2,3,4],'rotation',[1,4]);
+sv_1234_1_4=check_hopf(ev1234_1_4)'
+%% Permutation  (123)_0 (time shift 0)
+ev123=permfix('stst','pv123',n_osc,'v',...
+    'perm',[1,2,3],'rotation',[0,1]);
+sv_123=check_hopf(ev123)'
+%% Permutation (12)_0(34)_0:  1=2 (time shift 0),3=4 (time shift 0), 1=3 (time shift 1/2)
+ev12_34=[permfix('stst','pv12_0_1',n_osc,'v',...
+    'perm',[1,2],'rotation',[0,1]),...
+         permfix('stst','pv34_0_1',n_osc,'v',...
+         'perm',[3,4],'rotation',[0,1])];
+sv_12_34=check_hopf(ev12_34)'
+%% Permutation (12)_{1/2}(34)_0: 1=2 (time shift 1/2),3=4 (time shift 0)
+ev12s_34=[permfix('stst','p12_1_2',n_osc,'v',...
+    'perm',[1,2],'rotation',[1,2]),...
+          permfix('stst','p34_0_1',n_osc,'v',...
+          'perm',[3,4],'rotation',[0,1])];
+sv_12s_34=check_hopf(ev12s_34)';
+%% Permutation (12)_{1/4}: 1=2 (time shift 1/4) is sufficient
+ev12_1_4=permfix('stst','p12',n_osc,'v',...
+    'perm',[1,2],'rotation',[1,4]);
 sv_12_1_4=check_hopf(ev12_1_4)'
 %% Branch off toward different periodic solutions
 hopfsyms={ev1234_1_4,ev123,ev12s_34,ev12_34};
